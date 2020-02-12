947b4008325ddaaa7474a74f22d283e8047db9a6
JRMfer <43999123+JRMfer@users.noreply.github.com>
2020-02-12 02:03:23 +0100

    various level of volatility

--- Assignment 1/Julien/main.py
+++ Assignment 1/Julien/main.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import defaultdict
 from test_code_julien import BinTreeOption, BlackScholes
 
 
@@ -16,3 +19,25 @@ if __name__ == "__main__":
     bs_put_price = bs_eu.put_price()
     print("Binomial Put Price: ", tree_put_price)
     print("Black Scholes Put Price: ", bs_put_price)
+
+    sigmas = np.linspace(0.01, 0.99, 100)
+    trees_call_eu = [
+        BinTreeOption(N, T, S, s, r, K, market="EU", option_type="call")
+        for s in sigmas
+        ]
+    bs_eus = [BlackScholes(T, S, K, r, s) for s in sigmas]
+    
+    call_prices = defaultdict(list)
+    for tree, bs in zip(trees_call_eu, bs_eus):
+        call_prices["Binomial tree"].append(tree.determine_price())
+        call_prices["Black Scholes"].append(bs.call_price())
+
+    fig = plt.figure()
+    plt.plot(sigmas, call_prices["Binomial tree"], label="Binomial tree")
+    plt.plot(sigmas, call_prices["Black Scholes"], label="Black Scholes")
+    plt.xlabel("Volatility")
+    plt.ylabel("Price")
+    plt.title("European call option price for various levels of volatility")
+    plt.legend()
+    plt.show()
+    plt.close()
