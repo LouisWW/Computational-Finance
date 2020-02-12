c477579575cafbe39f16fbcb7f484bb6417f6b69
JRMfer <43999123+JRMfer@users.noreply.github.com>
2020-02-12 02:20:09 +0100

    increasing time steps bin tree

--- Assignment 1/Julien/main.py
+++ Assignment 1/Julien/main.py
@@ -5,6 +5,8 @@ from test_code_julien import BinTreeOption, BlackScholes
 
 
 if __name__ == "__main__":
+
+    # Test estimations
     N, T, S, K, r, sigma = 50, 1, 100, 99, 0.06, 0.2
     tree_call_eu = BinTreeOption(N, T, S, sigma, r, K, market="EU", option_type="call")
     bs_eu = BlackScholes(T, S, K, r, sigma)
@@ -20,6 +22,7 @@ if __name__ == "__main__":
     print("Binomial Put Price: ", tree_put_price)
     print("Black Scholes Put Price: ", bs_put_price)
 
+    # Analyse various levels of volatility
     sigmas = np.linspace(0.01, 0.99, 100)
     trees_call_eu = [
         BinTreeOption(N, T, S, s, r, K, market="EU", option_type="call")
@@ -32,12 +35,34 @@ if __name__ == "__main__":
         call_prices["Binomial tree"].append(tree.determine_price())
         call_prices["Black Scholes"].append(bs.call_price())
 
+    # Make plot
     fig = plt.figure()
     plt.plot(sigmas, call_prices["Binomial tree"], label="Binomial tree")
     plt.plot(sigmas, call_prices["Black Scholes"], label="Black Scholes")
     plt.xlabel("Volatility")
     plt.ylabel("Price")
     plt.title("European call option price for various levels of volatility")
+    plt.legend()
+    plt.show()
+    plt.close()
+
+
+    # Analyse time steps
+    steps = list(range(1, 100))
+    trees_call_eu = [
+        BinTreeOption(step, T, S, sigma, r, K, market="EU", option_type="call")
+        for step in steps
+        ]
+
+    prices_trees = [tree.determine_price() for tree in trees_call_eu]
+    prices_bs = [bs_call_price] * len(steps)
+    # Make plot
+    fig = plt.figure()
+    plt.plot(steps, prices_trees, label="Binomial tree")
+    plt.plot(steps, prices_bs, label="Black Scholes")
+    plt.xlabel("Time steps")
+    plt.ylabel("Price")
+    plt.title("European call option price for increasing time steps")
     plt.legend()
     plt.show()
     plt.close()
