e46447532ee68dd232af45833c266cdeb9c7570f
Louis Weyland <weylandlouis@gmail.com>
2020-02-23 16:17:06 +0100

    - created first files

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -6,3 +6,38 @@ Created on Friday Feb 20 2020
 This code was implemented by
 Louis Weyland, Floris Fok and Julien Fer
 """
+
+import helper as helper
+
+
+
+'''
+Basic Option Valuation : 
+-computing the discounted value of the average pay-off
+- convergence studies by increasing the number of trials and compare to assignment 1
+- vary the strike price and the volatility
+- estimate the Standard error and accuracy
+'''
+
+
+
+
+
+'''
+Estimation of Sensitivities in MC:
+-Compute bump-and-reveal mehtod 
+-Determine small delta 
+- use different/same seed for bumped/unbumped estimate of the value
+- and point 2 use sophisticated method discussed in the lecture
+'''
+
+
+'''
+Variance Reduction:
+- control variates technique on Asian option based on arithmetic average
+- study performance for different number of paths/strike/number of time points, etc.
+'''
+
+
+
+
