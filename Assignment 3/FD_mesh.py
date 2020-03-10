a25b9d9ceb408a4834a97034c9e5e6c4df742ba1
Louis Weyland <weylandlouis@gmail.com>
2020-03-10 16:52:23 +0100

    - Created a mesh with user-friendly printing

---
+++ Assignment 3/FD_mesh.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Created on Friday Feb 20 2020
+This code was implemented by
+Louis Weyland, Floris Fok and Julien Fer
+"""
+
+import numpy as np
+
+class FD_mesh:
+
+    def __init__(self, S_max,dS,T_max,dT):
+        self.S_max = S_max
+        self.dS = dS
+        self.T_max = T_max
+        self.dT = dT
+
+        # To make the grid from 0 to T_max/S_max
+        self.grid = np.zeros((self.T_max+1,self.S_max+1))
+
+    def __str__(self):
+
+        self.str="\n\n Finite Difference mesh \n\n"
+        for i in range(self.S_max):
+            for j in range(self.T_max):
+
+                self.str += str(self.grid[i][j])
+                self.str += "  "
+            self.str += "\n"
+
+        self.str += "\n\n\n\n"
+
+        return self.str
--- Assignment 3/helper.py
+++ Assignment 3/helper.py
@@ -6,3 +6,13 @@ Created on Friday Feb 20 2020
 This code was implemented by
 Louis Weyland, Floris Fok and Julien Fer
 """
+
+from FD_mesh import FD_mesh
+
+
+# FD_mesh(S_max,dS,T_max,dT)
+Grid = FD_mesh(10,1,10,1)
+
+print(Grid)
+
+
