2a04c34e99e7a34cf793e873f103d93b2018c133
florisfok <florisfok5@gmail.com>
2020-03-11 12:06:15 +0100

    unsuccessful PDE

--- Assignment 3/FD_mesh.py
+++ Assignment 3/FD_mesh.py
@@ -24,7 +24,6 @@ class FdMesh:
         self.t_max = t_max
         self.dt = dt
         self.n_steps_s = len(np.arange(self.s_min, self.s_max, self.ds))
-        print(self.n_steps_s)
         self.n_steps_t = int((self.t_max + 1) / dt)
 
         # To make the grid from 0 to T_max/S_max
@@ -34,42 +33,55 @@ class FdMesh:
         self.K = np.zeros(self.n_steps_s)
 
     def initialize_FTCS(self):
-        first = [{'value' : -1, 'offset' : 1}, {'value' : 1, 'offset' : -1}]
+        """Setup, boundarie conditions are set to aviod 100 variables"""
+        first = [{'value': -1, 'offset': 1}, {'value': 1, 'offset': -1}]
         second = [{'value': 2, 'offset': 0}, {'value': 1, 'offset': -1}, {'value': 1, 'offset': 1}]
 
+        # Extra term for boundarie in first order matrix approx
         extra1 = np.zeros(self.n_steps_s)
         extra1[-1] = np.exp(self.t_max)
 
+        # Extra term for boundarie in second order matrix approx
         extra2 = np.zeros(self.n_steps_s)
         extra2[-1] = np.exp(self.t_max) * (2/self.dt)
 
+        # second and first order approximations in  matrix form with boundaries given
         part1 = self.tri_diag_matrix_func(0, 0, 0, 0, first, printing=True)  * (1 / (self.ds * 2)) + extra1
         part2 = self.tri_diag_matrix_func(0, 0, 2, -2, second, printing=True)  * (1 / (self.ds ** 2)) + extra2
 
+        # K- term, interest and the A matrix price movement
         self.K += np.exp(self.r)
         self.A = (self.r - ((self.sigma ** 2) / 2)) * part1 + ((self.sigma ** 2) / 2) * part2
 
+        # First layer in the grid
         self.grid[:, 0] = np.arange(self.s_min, self.s_max, self.ds)[::-1]
 
+        # Show initial grid
         print("Initial")
         print(pd.DataFrame(self.grid))
         print(list(self.K))
 
 
     def forward_approximation(self, j):
+        '''Forward approximation using the matrixes'''
+        # Get old values and calculate new ones
         V_n = self.grid[:, j - 1]
         deltas = self.dt * (np.dot(V_n, self.A))
         V = V_n + (deltas + self.K)
 
+        # Save new values and delta
         self.grid[:, j] = V
         self.delta[:, j] = deltas
 
     def run(self):
+        # Setup
         self.initialize_FTCS()
 
+        # Loop, forward
         for j in range(1, self.grid.shape[1]):
             self.forward_approximation(j)
 
+        # show results
         print("Final")
         print(pd.DataFrame(self.grid))
 
